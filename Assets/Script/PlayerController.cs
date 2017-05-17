f32d8af504f1576bed0472f1f6346f888d824907
Makoto Ohnami <z.ohnami@gmail.com>
2017-05-17 23:20:44 +0900

    add player and camera

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	// speedを制御する
+	public float speed = 10;
+
+	void FixedUpdate () {
+		//  入力をxとzに代入
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		// 同一のGameObjectが持つRigidbodyコンポーネントを取得
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		// rigidbodyのx軸（横）とz軸（奥）に力を加える
+		rigidbody.AddForce(x * speed, 0, z * speed);
+	}
+}
