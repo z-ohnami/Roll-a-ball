695dfa43141376f137ddead290a9d9064b6f8084
Makoto Ohnami <z.ohnami@gmail.com>
2017-05-18 22:46:11 +0900

    add improvements

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public UnityEngine.UI.Text scoreLabel;
+	public GameObject winnerLabelObject;
+
+	public void Update ()
+	{
+		int count = GameObject.FindGameObjectsWithTag ("Item").Length;
+		scoreLabel.text = count.ToString ();
+		if (count == 0) {
+			winnerLabelObject.SetActive (true);
+		}
+	}
+}
